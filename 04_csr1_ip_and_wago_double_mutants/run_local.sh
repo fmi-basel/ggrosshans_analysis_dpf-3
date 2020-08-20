4553bdbf47a753ecf2bb3e8ff9b30c295c6005fa
Foivos Gypas <fgypas@gmail.com>
2020-08-20 11:22:15 +0200

    Add pipeline for csr1_ip and wago_double_mutants

---
+++ 04_csr1_ip_and_wago_double_mutants/run_local.sh
@@ -0,0 +1,15 @@
+#!/urs/bin/bash
+
+set -e
+
+mkdir -p logs/cluster_log
+mkdir -p logs/local_log
+
+snakemake \
+--use-conda \
+--conda-prefix ../conda/ \
+--configfile config.yaml \
+-p \
+--local-cores 60 \
+--cores 60 \
+--rerun-incomplete
